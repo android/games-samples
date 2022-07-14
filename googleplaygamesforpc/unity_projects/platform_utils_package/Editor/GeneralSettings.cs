4b27be69fb81e4899db217d00d38f5078b34e431
vlad-unity <vlad-unity@users.noreply.github.com>
2022-07-14 19:29:43 +0200

    Added: * GPG platform define script, that will add UNITY_ANDROID_x86_64 define to the list of custom defines withing Player settings * Asset importer script, that will set every imported texture to a desired texture compression - currently DXT by default * Setting window for the options available under Tools->GPG Settings

---
+++ googleplaygamesforpc/unity_projects/platform_utils_package/Editor/GeneralSettings.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#if UNITY_EDITOR && UNITY_ANDROID
+
+using UnityEditor;
+
+public class GeneralSettings
+{
+    public static bool IsSeparatePlatformDefineUsed = true;
+    public static string PlatformDefinePrefix = "UNITY_ANDROID_";
+    public static bool IsOverriddenForAndroid = true;
+    public static ImportFormats ImportFormat = ImportFormats.DXTC;
+    public static bool LoggingEnabled = true;
+    public static LogLevels LoggingLevel = LogLevels.Error;
+
+    public enum ImportFormats
+    {
+        None = TextureImporterFormat.RGBA32,
+        Low = TextureImporterFormat.DXT5,
+        DXTC = TextureImporterFormat.DXT1,
+        High = TextureImporterFormat.DXT1Crunched
+    }
+
+    public enum LogLevels
+    {
+        None,
+        Infrom,
+        Warning,
+        Error
+    }
+}
+
+#endif
---
+++ googleplaygamesforpc/unity_projects/platform_utils_package/Runtime/AssetImporter.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#if UNITY_EDITOR && UNITY_ANDROID
+
+using UnityEditor;
+using UnityEngine;
+
+public class AssetImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        // Check if this is the first import. If not, skip.
+        if (!assetImporter.importSettingsMissing)
+        {
+            return;
+        }
+
+        Log("First import of a texture, changing import settings.");
+
+        TextureImporter textureImporter = assetImporter as TextureImporter;
+
+        TextureImporterPlatformSettings platformSettings = new TextureImporterPlatformSettings
+        {
+            name = "Android",
+            format = (TextureImporterFormat) GeneralSettings.ImportFormat,
+            overridden = true
+        };
+        if (textureImporter != null) textureImporter.SetPlatformTextureSettings(platformSettings);
+    }
+
+    private void Log(string message)
+    {
+        if (GeneralSettings.LoggingEnabled)
+        {
+            Debug.Log("[ImportPreProcessor] " + message, assetImporter);
+        }
+    }
+}
+
+#endif
